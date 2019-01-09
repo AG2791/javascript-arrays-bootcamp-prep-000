efe274d3989802eed6b02419c53e3dc598e4a8e2
AJ Guiles <ashiguiles@gmail.com>
2019-01-09 03:05:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,11 +6,11 @@ function addElementToBeginningOfArray(array, element)  {
    
 }
   
-// function destructivelyAddElementToBeginningOfArray(array, element) 
-// {
-//   array.unshift(element);
-//   return array;
-// }
+function destructivelyAddElementToBeginningOfArray(array, element) 
+{
+  array.unshift(element);
+  return array;
+}
 
 
 
