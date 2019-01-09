9e2ff0033f6d522612b5eb92ea1c5697ad1fabc9
AJ Guiles <ashiguiles@gmail.com>
2019-01-09 03:06:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,48 +13,34 @@ function destructivelyAddElementToBeginningOfArray(array, element)
 }
 
 
-
-// function addElementToEndOfArray(array, element) {
+function addElementToEndOfArray(array, element) {
   
-//   return [...array, element];
+  return [...array, element];
   
-// }
-
-// function destructivelyAddElementToEndOfArray(array, element)
-// { 
-//   array.push(element);
-//   return array;
-// }
-
-
-// function accessElementInArray(array, index) {
-//   return  array [index];
-// }
-  
-
-// function destructivelyRemovelElementFromBeginningOfArray(array, element)
-// { 
-//   array.slice(element);
-//   return array;
-// }
-
-
-
-var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"]
-
-function addElementToBeginningOfArray(array,element){
-  return [element, ...array]
-
 }
 
-function destructivelyAddElementToBeginningOfArray(array,element){
-   array.unshift(element);
-    return array
+function destructivelyAddElementToEndOfArray(array, element)
+{ 
+  array.push(element);
+  return array;
 }
 
-function addElementToEndOfArray(array,element){
-  return [...array, element]
+
+function accessElementInArray(array, index) {
+  return  array [index];
 }
+  
+
+function destructivelyRemovelElementFromBeginningOfArray(array, element)
+{ 
+  array.slice(element);
+  return array;
+}
+
+
+
+
+
 
 function destructivelyAddElementToEndOfArray(array,element){
   array.push(element)
