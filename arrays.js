12b6401535da339a54c772f5d5c14c98592236b2
AJ Guiles <ashiguiles@gmail.com>
2019-01-09 04:15:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,16 +29,6 @@ function accessElementInArray(array, index) {
 }
   
 
-// function destructivelyRemovelElementFromBeginningOfArray(array)
-// { 
-//   // var array = [1,2,3]
-//   // [1,2,3]
-//   // array.shift();
-//   // return array.shift();
-//   array.shift();
-//   return array
-// }
-
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   array
