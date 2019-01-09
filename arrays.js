e305b616c371a4b536fe9ce75b8f24b1ed2d072a
AJ Guiles <ashiguiles@gmail.com>
2019-01-09 03:41:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,8 +18,7 @@ function addElementToEndOfArray(array, element) {
   
 }
 
-function destructivelyAddElementToEndOfArray(array, element)
-{ 
+function destructivelyAddElementToEndOfArray(array, element){ 
   array.push(element);
   return array;
 }
