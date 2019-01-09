eb10f5acca49bd7fa4178ae92982a977872ee037
AJ Guiles <ashiguiles@gmail.com>
2019-01-09 03:03:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,42 +1,83 @@
-var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
+// var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element)  {
+// function addElementToBeginningOfArray(array, element)  {
   
-  return [element, ...array]; 
+//   return [element, ...array]; 
    
-}
+// }
   
-function destructivelyAddElementToBeginningOfArray(array, element) 
-{
-  array.unshift(element);
-  return array;
-}
+// function destructivelyAddElementToBeginningOfArray(array, element) 
+// {
+//   array.unshift(element);
+//   return array;
+// }
 
 
 
-function addElementToEndOfArray(array, element) {
+// function addElementToEndOfArray(array, element) {
   
-  return [...array, element];
+//   return [...array, element];
   
-}
+// }
 
-function destructivelyAddElementToEndOfArray(array, element)
-{ 
-  array.push(element);
-  return array;
-}
+// function destructivelyAddElementToEndOfArray(array, element)
+// { 
+//   array.push(element);
+//   return array;
+// }
 
 
-function accessElementInArray(array, index) {
-  return  array [index];
-}
+// function accessElementInArray(array, index) {
+//   return  array [index];
+// }
   
 
-function destructivelyRemovelElementFromBeginningOfArray(array, element)
-{ 
-  array.slice(element);
-  return array;
+// function destructivelyRemovelElementFromBeginningOfArray(array, element)
+// { 
+//   array.slice(element);
+//   return array;
+// }
+
+
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"]
+
+function addElementToBeginningOfArray(array,element){
+  return [element, ...array]
+
 }
 
+function destructivelyAddElementToBeginningOfArray(array,element){
+   array.unshift(element);
+    return array
+}
 
+function addElementToEndOfArray(array,element){
+  return [...array, element]
+}
 
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array[index]
+}
+
+function removeElementFromEndOfArray(array){
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array){
+ return array.slice(1)
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length-1)
+}
